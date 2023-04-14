3e5f49f4939a8fc0ebd9e7089e0194fceea3f641
gustavopetrichhoepers <guhoepers25@gmail.com>
2023-04-14 11:07:51 -0300

    commit 6

--- src/App.vue
+++ src/App.vue
@@ -26,14 +26,14 @@ function sendForm() {
     dateBirth: clientInfo.value.dateBirth,
     address: clientInfo.value.address,
     city: clientInfo.value.city,
-    state: clientInfo.value.confirmstate,
+    state: clientInfo.value.confirmState,
     hobbies: clientInfo.value.hobbies,
     programmingLanguage: clientInfo.value.programmingLanguage,
     biography: clientInfo.value.biography
   }
 };
 
-const showpassword = ref(false);
+const showPassword = ref(false);
 const sentInfo = ref(null);
 
 const states = [
@@ -68,246 +68,182 @@ const states = [
 </script>
 
 <template>
- <div>
-    <form @submit.prevent="sendForm">
-      <div class="card text-bg-secondary mb-5 mt-5">
-        <div class="card-header text-bg-primary text-center">
+  <div>
+    <div>
+      <form @submit.prevent="sendForm">
+        <div class="card text-bg-secondary mb-5 mt-5">
+          <div class="card-header text-bg-primary text-center">
+            <h1>Form</h1>
+          </div>
+          <div class="card-body">
+            <h2 class="card-title">Enter the information:</h2>
+
+            <div class="form-group">
+              <label for="clientInfo.name">Name:</label>
+              <input type="text" v-model="clientInfo.name" class="form-control" required />
+            </div>
+
+            <div class="form-group">
+              <label for="clientInfo.email">E-mail:</label>
+              <input type="email" v-model="clientInfo.email" class="form-control" required />
+            </div>
+
+            <div class="form-group">
+              <label for="clientInfo.password" class="form-label">password</label>
+              <div class="input-group">
+                <input class="form-control" v-model="clientInfo.password" :type="showPassword ? 'text' : 'password'" />
+                <button class="btn btn-light" type="button" @click="showPassword = !showPassword">
+                  Mostrar
+                </button>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="passwordConfirm" class="form-label">Confirm the password</label>
+              <div class="input-group">
+                <input class="form-control" v-model="clientInfo.passwordConfirm"
+                  :type="showPassword ? 'text' : 'password'" />
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label for="clientInfo.dateBirth">Date of Birth:</label>
+              <input type="date" v-model="clientInfo.dateBirth" class="form-control" required />
+            </div>
+
+            <div>
+              <div class="form-group">
+                <label for="clientInfo.confirmState">State:</label>
+                <select v-model="clientInfo.confirmState" class="form-control" required>
+                  <option v-for="state in states" :key="state.acronym" :value="state.namestate">
+                    {{ state.namestate }}
+                  </option>
+                </select>
+              </div>
+
+
+              <div class="form-group">
+                <label for="clientInfo.city">City:</label>
+                <input type="text" v-model="clientInfo.city" class="form-control" required />
+              </div>
+
+              <div class="form-group">
+                <label for="clientInfo.address">Address:</label>
+                <input type="text" v-model="clientInfo.address" name="address" class="form-control" required />
+              </div>
+
+              <div class="form-group">
+                <label for="clientInfo.hobbies">Hobbies:</label>
+                <input type="text" v-model="clientInfo.hobbies" name="Hobbies" class="form-control" required />
+              </div>
+              <div class="form-group">
+                <label for="clientInfo.programmingLanguage">Programming Languages:</label>
+                <input type="text" v-model="clientInfo.programmingLanguage" name="Programming Languages"
+                  class="form-control" required />
+              </div>
+              <div class="form-group">
+                <label for="clientInfo.biography">Biography:</label>
+                <input type="text" v-model="clientInfo.biography" name="Biography" class="form-control"
+                  placeholder="Biography" required />
+              </div>
+              <div class="d-grid gap-2 mt-3">
+                <button class="btn btn-info" type="submit">
+                  Send
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <div v-if="sentInfo">
+      <div class="card text-bg-secondary ms-5 mb-5 mt-5 ">
+        <div class="card-header text-bg-warning text-center">
           <h1>Form</h1>
         </div>
         <div class="card-body">
-          <h2 class="card-title">Enter the information:</h2>
-
-          <div class="form-group">
-            <label for="clientInfo.name">Name:</label>
-            <input
-              type="text"
-              v-model="clientInfo.name"
-              class="form-control"
-              placeholder="Name"
-              required
-            />
-          </div>
-
-          <div class="form-group">
-            <label for="clientInfo.email">E-mail:</label>
-            <input
-              type="email"
-              v-model="clientInfo.email"
-              class="form-control"
-              placeholder="E-mail"
-              required
-            />
-          </div>
-
-          <div class="form-group">
-            <label for="clientInfo.password" class="form-label">password</label>
-            <div class="input-group">
-              <input
-                class="form-control"
-                v-model="clientInfo.password"
-                :type="showpassword ? 'text' : 'password'"
-              />
-              <button
-                class="btn btn-light"
-                type="button"
-                @click="showpassword = !showpassword"
-              >
-                Mostrar
-              </button>
-            </div>
-          </div>
-
-          <div class="form-group">
-            <label for="passwordConfirm" class="form-label">Confirm the password</label>
-            <div class="input-group">
-              <input
-                class="form-control"
-                v-model="clientInfo.passwordConfirm"
-                :type="showpassword ? 'text' : 'password'"
-              />
-            </div>
-          </div>
-
-          <div class="form-group">
-            <label for="clientInfo.dateBirth">Date of Birth:</label>
-            <input
-              type="date"
-              v-model="clientInfo.dateBirth"
-              class="form-control"
-              placeholder="Date of Birth"
-              required
-            />
-          </div>
-          
+          <h2 class="card-title">Your Info:</h2>
           <div>
-            <div class="form-group">
-              <label for="clientInfo.confirmstate">State:</label>
-              <select v-model="clientInfo.confirmstate" class="form-control" required>
-                
-                <option value="" disabled>Select the state</option>
-                <option v-for="state in states" :key="state.acronym" :value="state.namestate">
-                  {{ state.acronym }} - {{ state.namestate }}
-                </option>
-              </select>
-            </div>
-
-
-          <div class="form-group">
-            <label for="clientInfo.city">City:</label>
-            <input
-              type="text"
-              v-model="clientInfo.city"
-              class="form-control"
-              placeholder="City"
-              required
-            />
-          </div>
-
-          <div class="form-group">
-            <label for="clientInfo.address">Address:</label>
-            <input
-              type="text"
-              v-model="clientInfo.address"
-              name="address"
-              class="form-control"
-              placeholder="Address"
-              required
-            />
-          </div>
-
-            <div class="form-group">
-              <label for="clientInfo.hobbies">Hobbies:</label>
-              <input
-                type="text"
-                v-model="clientInfo.hobbies"
-                name="Hobbies"
-                class="form-control"
-                placeholder="Hobbies"
-                required
-              />
-            </div>
-            <div class="form-group">
-              <label for="clientInfo.programmingLanguage">Programming Languages:</label>
-              <input
-                type="text"
-                v-model="clientInfo.programmingLanguage"
-                name="Programming Languages"
-                class="form-control"
-                placeholder="Programming Languages"
-                required
-              />
-            </div>
-            <div class="form-group">
-              <label for="clientInfo.biography">Biography:</label>
-              <input
-                type="text"
-                v-model="clientInfo.biography"
-                name="Biography"
-                class="form-control"
-                placeholder="Biography"
-                required
-              />
-            </div>
-            <div class="d-grid gap-2 mt-3">
-              <button class="btn btn-info" type="submit" @click="sendForm">
-                Send
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </form>
-  </div>
-
-  <div v-if="sentInfo">
-    <div class="card text-bg-secondary ms-5 mb-5 mt-5 ">
-      <div class="card-header text-bg-warning text-center">
-        <h1>Form</h1>
-      </div>
-      <div class="card-body">
-        <h2 class="card-title">Your Info:</h2>
-        <div>
-          <ul class="list-group list-group-flush">
-            <label for="itemName">Name:</label>
-            <div>
-              <div class="form-control">
-                <li class="list-group-item" >{{ sentInfo.name }}</li>
-              </div>
-            </div>
-
-            <label for="itemName">Email:</label>
-            <div>
-              <div class="form-control">
-                <li class="list-group-item">{{ sentInfo.email }}</li>
-              </div>
-            </div>
-
-            <label for="itemName">Password:</label>
-            <div>
-              <div class="form-control">
-                <li class="list-group-item">{{ sentInfo.password }}</li>
-              </div>
-            </div>
-            <label for="itemName">Confirm:</label>
-            <div>
-              <div class="form-control">
-                <li class="list-group-item">{{ sentInfo.passwordConfirm }}</li>
-              </div>
-            </div>
-            <label for="itemName">Date of Birth:</label>
-            <div>
-              <div class="form-control">
-                <li class="list-group-item">{{ sentInfo.dateBirth }}</li>
-              </div>
-            </div>
-
-            <label for="itemName">State:</label>
-            <div class="form-control">
+            <ul class="list-group list-group-flush">
+              <label for="itemName">Name:</label>
               <div>
-                <li class="list-group-item">{{ sentInfo.state }}</li>
+                <div class="form-control">
+                  <li class="list-group-item">{{ sentInfo.name }}</li>
+                </div>
               </div>
-            </div>
 
-            <label for="itemName">City:</label>
-            <div>
-              <div class="form-control">
-                <li class="list-group-item">{{ sentInfo.city }}</li>
+              <label for="itemName">Email:</label>
+              <div>
+                <div class="form-control">
+                  <li class="list-group-item">{{ sentInfo.email }}</li>
+                </div>
               </div>
-            </div>
 
-            <label for="itemName">Address:</label>
-            <div>
-              <div class="form-control">
-                <li class="list-group-item">{{ sentInfo.address }}</li>
+              <label for="itemName">Password:</label>
+              <div>
+                <div class="form-control">
+                  <li class="list-group-item">{{ sentInfo.password }}</li>
+                </div>
               </div>
-            </div>
+              <label for="itemName">Confirm:</label>
+              <div>
+                <div class="form-control">
+                  <li class="list-group-item">{{ sentInfo.passwordConfirm }}</li>
+                </div>
+              </div>
+              <label for="itemName">Date of Birth:</label>
+              <div>
+                <div class="form-control">
+                  <li class="list-group-item">{{ sentInfo.dateBirth }}</li>
+                </div>
+              </div>
 
-            <label for="itemName">Hobbies:</label>
-            <div>
+              <label for="itemName">State:</label>
               <div class="form-control">
-                <li class="list-group-item">{{ sentInfo.hobbies }}</li>
+                <div>
+                  <li class="list-group-item">{{ sentInfo.state }}</li>
+                </div>
               </div>
-            </div>
-            <label for="itemName">Programming Languages:</label>
-            <div>
-              <div class="form-control">
-                <li class="list-group-item">{{ sentInfo.programmingLanguage }}</li>
+
+              <label for="itemName">City:</label>
+              <div>
+                <div class="form-control">
+                  <li class="list-group-item">{{ sentInfo.city }}</li>
+                </div>
               </div>
-            </div>
-            <label for="itemName">Biography:</label>
-            <div>
-              <div class="form-control">
-                <li class="list-group-item">{{ sentInfo.biography }}</li>
+
+              <label for="itemName">Address:</label>
+              <div>
+                <div class="form-control">
+                  <li class="list-group-item">{{ sentInfo.address }}</li>
+                </div>
               </div>
-            </div>
-          </ul>
+
+              <label for="itemName">Hobbies:</label>
+              <div>
+                <div class="form-control">
+                  <li class="list-group-item">{{ sentInfo.hobbies }}</li>
+                </div>
+              </div>
+              <label for="itemName">Programming Languages:</label>
+              <div>
+                <div class="form-control">
+                  <li class="list-group-item">{{ sentInfo.programmingLanguage }}</li>
+                </div>
+              </div>
+              <label for="itemName">Biography:</label>
+              <div>
+                <div class="form-control">
+                  <li class="list-group-item">{{ sentInfo.biography }}</li>
+                </div>
+              </div>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  
-
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
